e4384de7f530b36b2d0e2734e09fd2c34c1993b6
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-07-09 23:42:14 +0600

    acceptable-content-type-check

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {   
-    function DemoAction(Request $request):array {
-        return $request->getAcceptableContentTypes();
+    function DemoAction(Request $request):string {
+        if ($request->accepts(['text/html'])) {
+            return "true";
+        } else {
+            return false;
+        }
     }
 }

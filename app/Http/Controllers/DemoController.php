4a87ea2025cb64aa67469c998f95d94f82c7c395
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-07-09 23:27:38 +0600

    ip-address

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,12 +6,7 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {   
-    function DemoAction(Request $request):bool {
-        $photoFile = $request->file('photo');
-        // storage directory photo file upload
-        $photoFile->storeAs('upload',$photoFile->getClientOriginalName());
-        // public directory photo file upload
-        $photoFile->move(public_path('upload'),$photoFile->getClientOriginalName());
-        return true;
+    function DemoAction(Request $request):string {
+        return $request->ip();
     }
 }

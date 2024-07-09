2a21cd8e2edb8961e97ef1459dd783ef4f1deeaa
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-07-09 22:51:49 +0600

    photo-file-upload-public-and-storage-directory

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,19 +6,12 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {   
-    function DemoAction(Request $request):array {
+    function DemoAction(Request $request):bool {
         $photoFile = $request->file('photo');
-        $fileSize = filesize($photoFile);
-        $fileType = fileType($photoFile);
-        $originalName = $photoFile->getClientOriginalName();
-        $tempFileName = $photoFile->getFilename();
-        $extension = $photoFile->extension();
-        return array(
-            'fileSize' => $fileSize,
-            'fileType' => $fileType,
-            'originalName' => $originalName,
-            'tempFileName' => $tempFileName,
-            'extension' => $extension,
-        );
+        // storage directory photo file upload
+        $photoFile->storeAs('upload',$photoFile->getClientOriginalName());
+        // public directory photo file upload
+        $photoFile->move(public_path('upload'),$photoFile->getClientOriginalName());
+        return true;
     }
 }
